767fe87d630313ca397809da50e8f18245b4ff58
Anurag Bhandari <anurag.bhd@gmail.com>
2020-01-18 19:43:35 +0530

    Add DB methods to execute statements and queries

--- db/db-lib.js
+++ db/db-lib.js
@@ -22,6 +22,52 @@ async function executeCommand(command) {
 }
 
 /**
+ * Executes the given parameterized DML statement.
+ *
+ * Does not validate whether the given statement is indeed a DML statement.
+ *
+ * @param {String} statement A data manipulation language statement,
+ * such as `INSERT` and `UPDATE`.
+ *
+ * @returns {Promise} A `Statement` object that represents result of the execution.
+ */
+async function executeStatement(statement) {
+  try {
+    const db = await sqlite.open(process.env.SQLITE_DB_PATH);
+    const preparedStatement = await db.prepare(statement); // this step is required for statements with params
+    const result = await preparedStatement.run();
+    await preparedStatement.finalize();
+    await db.close();
+    return result;
+  } catch (e) {
+    throw Error(e);
+  }
+}
+
+/**
+ * Executes the given parameterized DQL statement.
+ *
+ * Does not validate whether the given statement is indeed a DQL statement.
+ *
+ * @param {String} query A data query language statement, that is `SELECT`.
+ *
+ * @returns {Promise} An array of objects that represent rows returned
+ * as the result of query execution.
+ */
+async function executeQuery(query) {
+  try {
+    const db = await sqlite.open(process.env.SQLITE_DB_PATH);
+    const preparedStatement = await db.prepare(query); // this step is required for statements with params
+    const result = await preparedStatement.all();
+    await preparedStatement.finalize();
+    await db.close();
+    return result;
+  } catch (e) {
+    throw Error(e);
+  }
+}
+
+/**
  * Applies SQL migrations contained inside homonymous directory.
  *
  * @returns {Promise}
@@ -40,5 +86,7 @@ async function migrateDb() {
 
 module.exports = {
   executeCommand,
+  executeStatement,
+  executeQuery,
   migrateDb
 };
--- test/db.test.js
+++ test/db.test.js
@@ -1,10 +1,38 @@
+const fs = require('fs');
 const assert = require('assert');
 const sqlite = require('sqlite3');
 const SQL = require('sql-template-strings');
 
-process.env.SQLITE_DB_PATH = ':memory:';
+process.env.SQLITE_DB_PATH = './db/generated-codes.test.db';
 const dbLib = require('../db/db-lib');
 
+before(deleteDb);
+after(deleteDb);
+
+/**
+ * Deletes the SQLite database file used by test environment.
+ */
+function deleteDb() {
+  if (fs.existsSync(process.env.SQLITE_DB_PATH)) {
+    fs.unlinkSync(process.env.SQLITE_DB_PATH);
+  }
+}
+
+/**
+ * Adds some seed data to help in testing.
+ */
+async function seedData() {
+  await dbLib.migrateDb();
+  const code = '111111';
+  const ts = new Date().toISOString();
+  await dbLib.executeStatement(
+    SQL`INSERT INTO unique_codes VALUES ( ${code}, ${ts} )`
+  );
+}
+
+/**
+ * Suite of tests.
+ */
 describe('DB lib', function() {
   describe('executeCommand()', function() {
     it('should return an error when an invalid statement is given', async function() {
@@ -23,6 +51,116 @@ describe('DB lib', function() {
         )
       `);
       assert.equal(result.changes, 0);
+    });
+  });
+
+  describe('executeStatement()', function() {
+    before(async function() {
+      deleteDb();
+      await seedData();
+    });
+
+    after(deleteDb);
+
+    it('should successfully add a new row', async function() {
+      const code = '123456';
+      const ts = new Date().toISOString();
+      const statement = SQL`INSERT INTO unique_codes VALUES ( ${code}, ${ts} )`;
+
+      const result = await dbLib.executeStatement(statement);
+
+      assert.equal(result.changes, 1);
+      assert.equal(result.sql, `INSERT INTO unique_codes VALUES ( ?, ? )`);
+    });
+
+    it('should successfully update an existing row', async function() {
+      const code = '123456';
+      const newCode = '654321';
+      const statement = SQL`UPDATE unique_codes SET code = ${newCode} WHERE code = ${code}`;
+
+      const result = await dbLib.executeStatement(statement);
+
+      assert.equal(result.changes, 1);
+      assert.equal(
+        result.sql,
+        `UPDATE unique_codes SET code = ? WHERE code = ?`
+      );
+    });
+
+    it('should successfully delete an existing row', async function() {
+      const code = '654321';
+      const statement = SQL`DELETE FROM unique_codes WHERE code = ${code}`;
+
+      const result = await dbLib.executeStatement(statement);
+
+      assert.equal(result.changes, 1);
+      assert.equal(result.sql, `DELETE FROM unique_codes WHERE code = ?`);
+    });
+
+    it('should throw error when an incomplete statement is given', async function() {
+      const statement = 'INSERT';
+
+      try {
+        await dbLib.executeStatement(statement);
+      } catch (e) {
+        assert.notEqual(e.message.indexOf('SQLITE_ERROR'), -1);
+      }
+    });
+
+    it('should throw error when an invalid statement is given', async function() {
+      const nonExistentCode = '999999';
+      const statement = SQL`DELETE FROM unique_codes WHERE code = ${nonExistentCode}`;
+
+      try {
+        await dbLib.executeStatement(statement);
+      } catch (e) {
+        assert.notEqual(e.message.indexOf('SQLITE_ERROR'), -1);
+      }
+    });
+  });
+
+  describe('executeQuery()', function() {
+    before(async function() {
+      deleteDb();
+      await seedData();
+    });
+
+    after(deleteDb);
+
+    it('should return all existing codes', async function() {
+      const query = SQL`SELECT * FROM unique_codes`;
+
+      const result = await dbLib.executeQuery(query);
+
+      assert.equal(result.length, 1);
+    });
+
+    it('should return a specific existing code', async function() {
+      const query = SQL`SELECT * FROM unique_codes WHERE code = '111111'`;
+
+      const result = await dbLib.executeQuery(query);
+
+      assert.equal(result.length, 1);
+    });
+
+    it('should throw error when an incomplete query is given', async function() {
+      const query = 'SELECT';
+
+      try {
+        await dbLib.executeQuery(query);
+      } catch (e) {
+        assert.notEqual(e.message.indexOf('SQLITE_ERROR'), -1);
+      }
+    });
+
+    it('should throw error when an invalid query is given', async function() {
+      const queryWithInvalidCol = SQL`SELECT code, created_by FROM unique_codes`;
+
+      try {
+        await dbLib.executeQuery(queryWithInvalidCol);
+      } catch (e) {
+        assert.notEqual(e.message.indexOf('SQLITE_ERROR'), -1);
+      }
     });
   });
 
