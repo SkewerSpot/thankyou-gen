0a8b81a4c057454ce232f210771a6be8b8e01d46
Anurag Bhandari <anurag.bhd@gmail.com>
2020-01-23 05:54:50 +0530

    Implement API endpoint for generating unique codes

--- api.js
+++ api.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const DbLib = require('./db/db-lib');
+const Util = require('./util');
 
-router.post('/unique-codes', (req, res) => {
-  res.json({ message: 'Haha' });
+/**
+ * Returns an array of `count` unique codes.
+ *
+ * The API endpoint optionally espects two query string params:
+ * - count: The desired number of unique codes to generate (bet. 1-1000).
+ * - dummy: If 'true', return dummy codes. Otherwise, generate from database.
+ *
+ * Sample usage:
+ * - /unique-codes?count=100
+ * - /unique-codes?count=100&dummy=true
+ */
+router.get('/unique-codes', async (req, res) => {
+  let count = 0;
+  let dummyMode = false;
+  let codes = [];
+
+  count = parseInt(req.query.count);
+  if (isNaN(count) || count < 0 || count > 1000) {
+    return res.status(400).json({
+      error: '`count` query param must be an integer between 0-1000'
+    });
+  }
+
+  dummyMode = req.query.dummy === 'true';
+
+  if (dummyMode) {
+    codes = Util.generateDummyUniqueCodes(count);
+  } else {
+    codes = await DbLib.generateUniqueCodes(count);
+  }
+
+  res.json(codes);
 });
 
 module.exports = router;
